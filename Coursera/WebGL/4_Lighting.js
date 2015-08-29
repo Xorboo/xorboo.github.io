bb118797fc373eacfaf74cd602ab9c921aed1ad0
Xorboo <xorboo@gmail.com>
2015-08-30 00:18:53 +0300

    Added color to animated light

--- Coursera/WebGL/4_Lighting.js
+++ Coursera/WebGL/4_Lighting.js
@@ -51,7 +51,7 @@ var lights =
     { // Animated light
         pos: vec4(-1.0, 0.5, 1.0, 0.0),
         ambient: vec4(0.3, 0.3, 0.3, 1.0),
-        diffuse: vec4(1.0, 1.0, 1.0, 1.0),
+        diffuse: vec4(1.0, 0.2, 0.2, 1.0),
         specular: vec4(1.0, 1.0, 1.0, 1.0),
         enabled: true
     }
